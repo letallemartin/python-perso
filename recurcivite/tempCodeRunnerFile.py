5180d823062de277c2e9ab49c8733debb1ffb2e9
letallemartin <letallemartin@gmail.com>
2026-08-11 15:21:37 +0200

    fibonachi limite

--- recurcivite/tempCodeRunnerFile.py
+++ recurcivite/tempCodeRunnerFile.py
@@ -1,7 +1,7 @@
 def fibonachi(nombre,ancien,lim,n = 0):
     if n < lim:
         n += 1
-        return fibonachi(nombre + ancien, nombre ,lim, n)
+        return fibonachi(nombre + ancien, nombre ,lim)
     return nombre
 print(fibonachi(1,0,5))
 
