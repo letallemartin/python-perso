85ff2c6b4b868a07cf26bfa30c50a2306abe0a7b
letallemartin <letallemartin@gmail.com>
2026-08-27 11:34:05 +0200

    fibonachi factorielle

---
+++ recurcivité.py
@@ -0,0 +1,13 @@
+# def factoriel(nbr):
+# 	if nbr > 1:
+# 		return nbr * factoriel(nbr - 1)
+# 	return nbr
+
+# print(factoriel(5))
+
+# def fibonachi(act, prc, n):
+# 	if n > 1:
+# 		return fibonachi(act + prc, act, n - 1)
+# 	return act
+
+# print("actuel = ", fibonachi(1, 0, 15))
